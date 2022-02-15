e36614c40657ace947309bdae24bde5c734ae716
jhdavidesouza <jhdavidesouza@gmail.com>
2022-02-15 07:55:43 -0300

    adding some folders to practice literals in Java

---
+++ learning-literals/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, World!");
+        /*
+        * This is a simple printer, printing "Hello, World!"
+        * */
+    }
+}
